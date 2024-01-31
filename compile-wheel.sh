1afd2b208c9013b4d9c1a00ee3c39bfca97d9a3c
zzb66666666x <18767066589@163.com>
2024-01-31 01:30:39 -0600

    fix compile ray script

--- compile-wheel.sh
+++ compile-wheel.sh
@@ -5,18 +5,20 @@ ray_path = $1
 cd $ray_path
 
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.0/install.sh | bash
+
+source ~/.bashrc
+
 nvm install 14
 nvm use 14
 
 # install bazel, run ./ci/env/install-bazel.sh
 ./ci/env/install-bazel.sh
 
-cd ./dashboard/client
+cd $ray_path/dashboard/client
 npm ci
 npm run build
-cd ../../
+cd $ray_path/python
 
-cd ./python
 pip3 install -e . --verbose
 
 # pack ray wheel file
